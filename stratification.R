abc012cdde41c527f4114f67fe9e97bff75363e1
Federico <gianoli.federico@gmail.com>
2022-03-02 08:59:43 +0100

    Update stratification.R
    update stratification

--- stratification.R
+++ stratification.R
@@ -1,42 +1,61 @@
-#library(terra)
+
 library(raster)
 library(sf)
-library(exactextractr)
-library(rgdal)
+library(sp)
 
 
 dirctry_data <- "F:/coe1000/melanie/indicators"
 output_data <- "F:/coe1000/melanie/stratificati_R"
 
 LC <- raster("F:\\coe1000\\melanie\\indicators\\LC\\LC.tif")
-#crs(LC) <- sp::CRS("EPSG:3035")
-#sp::CRS("EPSG:3035")
-#crs(LC)<-CRS("+init=epsg:3035")
+
+##If the CRS is not assigned
+crs(LC) <- st_crs(3035)$wkt
+## see https://inbo.github.io/tutorials/tutorials/spatial_crs_coding/
+
+LC
+
+#issue <- raster("F:\\coe1000\\melanie\\indicators\\POPULATION\\pop_250m.tif")
+#issue <- raster("F:\\coe1000\\melanie\\indicators\\RUSLE\\rusle_input_1000.tif")
+#issue <- raster("F:\\coe1000\\melanie\\indicators\\ACIDIFICATION\\eacidification_1000.tif")
+#issue <- raster("F:\\coe1000\\melanie\\indicators\\EUTROFICATION\\eutrofication_1000.tif")
+#issue <- raster("F:\\coe1000\\melanie\\indicators\\WIND\\wind_1000.tif")
+issue <- raster("F:\\coe1000\\melanie\\indicators\\SOIL_MICRO\\soilmicro_1000.tif")
+#issue <- raster("F:\\coe1000\\melanie\\indicators\\POP_CHANGE\\pop_change.tif")
 
 
-issue <- raster("F:\\coe1000\\melanie\\indicators\\POPULATION\\pop_250m.tif")
+crs(issue) <- st_crs(3035)$wkt 
+issue
+plot(issue)
 
+### Check Median with SHP
 #LC_shp <- st_read("F:\\coe1000\\melanie\\indicators\\LC\\LC_dissolved.shp")
 #
-#LC_shp$Median <- exact_extract(builtup, LC_shp, 'median')
+#LC_shp$Median <- exact_extract(issue, LC_shp, 'median')
 #LC_shp$Median
 #LC2 = LC_shp|>st_drop_geometry() ## per togliere la geometria
+#[1] 20.34540023  0.96096892  1.48573614  1.10332930  0.02661822  0.08506864
 
 
-issue <- crop(issue, extent(LC))  #taglia il raster A sull'extent del raster B
+### IF the two input raster are not aligned
+##Il file del rusle non era allineato e allora ho applicato il resample
+x<-resample(issue,LC, method='ngb')
+x
+issue <-x
+
+
+issue <- crop(issue, extent(LC))  
 res(issue) <- res(LC) 
 extent(issue) <- extent(LC)
        
-
+issue
 #crs(LC_shp)
 
 s<- issue
 r<- LC
 result <- zonal(s, r, 'median')
 result
-
-## Result population
-
+#plot(issue)
 
 #reclassify(r, result, file.path(dirctry_data, "LC_reclassified.tif"))
 
@@ -54,7 +73,7 @@ s2 <- stack(s,r2)
 
 
 beginCluster(6) ##ASSIGN CORE
-z1 <- clusterR(s2, calc, args=list(fun=rc), filename=file.path(output_data, "population_issue_R.tif"), datatype = "INT1U", overwrite = TRUE)
+z1 <- clusterR(s2, calc, args=list(fun=rc), filename=file.path(output_data, "popchange_R.tif"), datatype = "INT1U", overwrite = TRUE)
 endCluster()
 
 
@@ -62,4 +81,6 @@ plot(z1)
 z1
 
 #writeRaster(x = z1, paste0(dirctry_data, "/rusleR_R.tif"), format = 'GTiff', overwrite = TRUE)
+
+
 
